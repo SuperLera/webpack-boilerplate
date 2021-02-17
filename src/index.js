46a83c15091f93f599e1648ac2a75ef320961126
Valeria Koroleva <valeriia.koroleva@gmail.com>
2021-02-17 17:54:07 +0300

    init: webpack config

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import Post from '@models/Post'
+import json from './assets/json.json'
+import xml from './assets/data.xml'
+import csv from './assets/data.csv'
+import WebpackLogo from './assets/webpack-logo.png'
+import './styles/styles.css'
+
+const post = new Post ('webpack post title', WebpackLogo)
+
+console.log('Post to String:', post.toString())
+
+// console.log('JSON:', json)
+
+// console.log('XML:', xml)
+
+// console.log('CSV:', csv)
